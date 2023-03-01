7731bd2d876e05e6b91e14d2f6653aa5c33be762
Mateo Sierra <m.sierra1037@hotmail.com>
2023-03-01 10:03:16 -0600

    advice app markup and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+interface Slip {
+  slip : {
+    id : number 
+    advice : string
+  }
+}
 
 function App() {
+
+
+  const [advice, setAdvice] = React.useState<Slip>()
+
+    async function getAdvice(){
+      const request = await fetch("https://api.adviceslip.com/advice")
+      const response = await request.json().then((data) =>{setAdvice(data)})
+      console.log("async func")
+    }
+
+  React.useEffect(()=>{
+    getAdvice()
+    console.log("useEffect");
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <section className="advice-card">
+        <h4 className="advice-card__advice-id">{advice ? advice.slip.id : "ADVICE # 000"}</h4>
+
+        <p className="advice-card__advice">{advice?.slip.advice}</p>
+
+        <hr />
+
+        <button 
+        onClick={getAdvice}
+        className="advice-card__reroll-buton">
+          rollerino</button>
+
+      </section>
     </div>
   );
 }
